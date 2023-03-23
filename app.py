56aeebf577a119336dada488b7a3a552c70fc284
Ian McKechnie <imckechn@uoguelph.ca>
2023-03-23 14:04:36 -0400

    Now creates a logs bucket and log file each time the algo runs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import json
 import boto3
+from datetime import datetime
 
 aws_access_key_id = "x"
 aws_secret_access_key = "x"
@@ -7,6 +7,8 @@ client_res = boto3.resource('s3', region_name='ca-central-1', aws_access_key_id=
 client = boto3.client('s3', region_name='ca-central-1', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 def lambda_handler(event, context):
+
+    logFileBody = ""
 
     # Get the object from the event and show its content type
     newFilebucket = event['Records'][0]['s3']['bucket']['name']
@@ -20,30 +22,63 @@ def lambda_handler(event, context):
     try:
         newFileUploaded = client.get_object(Bucket=newFilebucket, Key=newFilekey)
         print("CONTENT TYPE: " + newFileUploaded['ContentType'])
+        logFileBody += "Found new file: " + newFilekey + "\n"
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
 
+
+    #Create the logfile bucket if it doesn't exist
+    logFileBucketName = "imckechnlogs"
+    try:
+        client_res.meta.client.head_bucket(Bucket=logFileBucketName)
+    except:
+        client.create_bucket(
+            Bucket=logFileBucketName,
+            CreateBucketConfiguration={
+                'LocationConstraint': 'ca-central-1',
+            },
+        )
+
+
     #Get the subscription list
     subscriptionList = client.get_object(Bucket='subscribercollectionianmckechnie', Key='subscribers')
-
     subs = subscriptionList['Body'].read()
 
     for subBucket in subs.splitlines():
-        subBucket = subBucket.decode()
-
-        #Check if the bucket exists, if not, create it
-        print("bucket =", subBucket)
         try:
-            client_res.meta.client.head_bucket(Bucket=subBucket)
-        except:
-            client.create_bucket(
-                Bucket='subBucket',
-                CreateBucketConfiguration={
-                    'LocationConstraint': 'ca-central-1',
-                },
-            )
+            subBucket = subBucket.decode()
 
-        #Copy the s3 object over to the subscriber
-        client_res.meta.client.copy(copy_source, subBucket, newFilekey)
+            #Check if the bucket exists, if not, create it
+            try:
+                client_res.meta.client.head_bucket(Bucket=subBucket)
+            except:
+                client.create_bucket(
+                    Bucket=subBucket,
+                    CreateBucketConfiguration={
+                        'LocationConstraint': 'ca-central-1',
+                    },
+                )
+
+            #Copy the s3 object over to the subscriber
+            client_res.meta.client.copy(copy_source, subBucket, newFilekey)
+            logFileBody += "Copied file to " + subBucket + "\n"
+
+        except:
+            logFileBody += "Error copying file to " + subBucket + "\n"
+
+
+
+    #Write the logfile
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    time = current_time.replace(":", "")
+    logFileKey = time + ".log"
+
+    object = client_res.Object(
+        bucket_name=logFileBucketName,
+        key=logFileKey
+    )
+
+    object.put(Body=logFileBody)
